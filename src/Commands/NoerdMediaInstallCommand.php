85443812d2de309265918e95bace05ea5f3ad38f
Johannes Brock <info@johannes-brock,de>
2025-08-28 18:19:24 +0200

    noerd

--- src/Commands/NoerdMediaInstallCommand.php
+++ src/Commands/NoerdMediaInstallCommand.php
@@ -18,7 +18,7 @@ class NoerdMediaInstallCommand extends Command
         $this->info('Installing noerd media content...');
 
         $sourceDir = base_path('vendor/noerd/media/content');
-        $targetDir = base_path('content');
+        $targetDir = storage_path('environment');
 
         if (!is_dir($sourceDir)) {
             $this->error("Source directory not found: {$sourceDir}");
