f726102a55c43375e52bcdae2738ef3ebcc19294
Paul Z. <paul@nywerk.de>
2025-12-05 18:58:51 +0100

    remove legacy middleware

--- routes/media-routes.php
+++ routes/media-routes.php
@@ -5,7 +5,7 @@ use Livewire\Volt\Volt;
 
 Route::prefix('media')
     ->as('media.')
-    ->middleware(['auth', 'verified', 'web', 'media'])
+    ->middleware(['auth', 'verified', 'web'])
     ->group(function (): void {
         Volt::route('/dashboard', 'media-list')->name('dashboard');
     });
--- src/Providers/MediaServiceProvider.php
+++ src/Providers/MediaServiceProvider.php
@@ -5,7 +5,6 @@ namespace Noerd\Media\Providers;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Volt\Volt;
 use Noerd\Media\Commands\NoerdMediaInstallCommand;
-use Noerd\Media\Middleware\MediaMiddleware;
 
 class MediaServiceProvider extends ServiceProvider
 {
@@ -20,9 +19,6 @@ class MediaServiceProvider extends ServiceProvider
 
         // Publish/merge configuration
         $this->mergeConfigFrom(__DIR__.'/../../config/media.php', 'media');
-
-        $router = $this->app['router'];
-        $router->aliasMiddleware('media', MediaMiddleware::class);
 
         Volt::mount(__DIR__.'/../../resources/views/livewire');
 
