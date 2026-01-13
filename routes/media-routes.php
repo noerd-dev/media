da0a1b9e64af5d3b051866108f82d94b2df548f4
Johannes Brock <info@johannes-brock.de>
2026-01-13 18:41:51 +0100

    add app access middleware

--- routes/media-routes.php
+++ routes/media-routes.php
@@ -5,7 +5,7 @@ use Livewire\Volt\Volt;
 
 Route::prefix('media')
     ->as('media.')
-    ->middleware(['auth', 'verified', 'web'])
+    ->middleware(['auth', 'verified', 'web', 'app-access:media'])
     ->group(function (): void {
         Volt::route('/dashboard', 'media-list')->name('dashboard');
     });
