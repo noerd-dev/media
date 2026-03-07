d65cf6be3854b034abf4a7905f673402050253c9
Johannes Brock <info@johannes-brock.de>
2026-03-07 18:29:01 +0100

    pint

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -5,9 +5,9 @@ namespace Noerd\Media\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Noerd\Media\Database\Factories\MediaFactory;
 use Noerd\Traits\BelongsToTenant;
 use Noerd\Traits\HasListScopes;
-use Noerd\Media\Database\Factories\MediaFactory;
 use Nywerk\Uki\Models\TextDocument;
 
 class Media extends Model
